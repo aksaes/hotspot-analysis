80d40c64934b29ef80c7907a7ecc8b34787a57c0
Aksa Elizabeth Sunny <aksaelizabeth28@gmail.com>
2023-04-12 15:59:00 -0700

    Add PageRank implementation

--- interface.py
+++ interface.py
@@ -13,6 +13,40 @@ class Interface:
         raise NotImplementedError
 
     def pagerank(self, max_iterations, weight_property):
-        # TODO: Implement this method
-        raise NotImplementedError
+
+        graph_name = 'trip_graph'
+
+        with self._driver.session() as session:
+            # Delete graph if it exists already
+            session.run(f"""
+                CALL gds.graph.drop('{graph_name}', false)
+            """)
+
+            # Create graph
+            session.run(f"""
+                CALL gds.graph.project(
+                    '{graph_name}',
+                    'Location',
+                    'TRIP',
+                    {{
+                        relationshipProperties: '{weight_property}'
+                    }}
+                );
+            """)
+
+            # Run PageRank
+            result = session.run(f'''
+                CALL gds.pageRank.stream('{graph_name}', {{
+                    maxIterations: {max_iterations},
+                    relationshipWeightProperty: '{weight_property}'
+                }})
+                YIELD nodeId, score
+                RETURN gds.util.asNode(nodeId).name as name, score
+                ORDER BY score DESC, name ASC
+            ''').data()
+
+            max_node = result[0]
+            min_node = result[-1]
+
+            return max_node, min_node
 
