b0a6433a00af86b476f48a1a055ce94a2bce7ffb
Aksa Elizabeth Sunny <aksaelizabeth28@gmail.com>
2023-04-25 10:27:22 -0700

    Add latest template code

--- data_producer.py
+++ data_producer.py
@@ -1,3 +1,4 @@
+# Some parts of the file may need to be modified to suit your environment
 from confluent_kafka import Producer
 import pyarrow.parquet as pq
 import time
@@ -5,7 +6,7 @@ import pandas as pd
 
 # set the topic name
 topic_name = "nyc_taxicab_data"
-conf = {'bootstrap.servers': 'localhost:30092'}
+conf = {'bootstrap.servers': 'localhost:9092'}
 
 # create a Kafka producer instance
 producer = Producer(conf)
@@ -20,17 +21,25 @@ trips = trips.to_pandas()
 
 # iterate over each row in the table and send it to Kafka
 for index, row in trips.iterrows():
+
+    # select a subset of columns
+    row = row[['trip_distance', 'PULocationID', 'DOLocationID', 'fare_amount']]
+
+    # convert the row to a JSON string
+    row = row.to_json()
+    
     # encode the row as bytes
     message = str(row).encode('utf-8')
 
     # send the message to Kafka
     producer.produce(topic_name, value=message)
+    producer.flush()
 
     # print
     print("Message sent to Kafka: {}".format(message))
 
-    # wait for 0.1 seconds
-    time.sleep(0.1)
+    # wait for 0.25 seconds
+    time.sleep(0.25)
 
 # close the producer connection
 producer.close()
