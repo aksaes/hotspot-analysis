31bbd1f37800e522f8041068a0dd5267ca616ee1
Aksa Elizabeth Sunny <aksaelizabeth28@gmail.com>
2023-05-05 21:36:48 -0700

    Update producer to use subset

--- data_producer.py
+++ data_producer.py
@@ -19,6 +19,21 @@ print("-----------------------------")
 trips = pq.read_table('yellow_tripdata_2022-03.parquet')
 trips = trips.to_pandas()
 
+# Some data cleaning and filtering
+trips = trips[['tpep_pickup_datetime', 'tpep_dropoff_datetime', 'PULocationID', 'DOLocationID', 'trip_distance', 'fare_amount']]
+
+# Filter out trips that are not in bronx
+bronx = [3, 18, 20, 31, 32, 46, 47, 51, 58, 59, 60, 69, 78, 81, 94, 119, 126, 136, 147, 159, 167, 168, 169, 174, 182, 183, 184, 185, 199, 200, 208, 212, 213, 220, 235, 240, 241, 242, 247, 248, 250, 254, 259]
+trips = trips[trips.iloc[:, 2].isin(bronx) & trips.iloc[:, 3].isin(bronx)]
+trips = trips[trips['trip_distance'] > 0.1]
+trips = trips[trips['fare_amount'] > 2.5]
+
+# Convert date-time columns to supported format
+trips['tpep_pickup_datetime'] = pd.to_datetime(trips['tpep_pickup_datetime'], format='%Y-%m-%d %H:%M:%S')
+trips['tpep_dropoff_datetime'] = pd.to_datetime(trips['tpep_dropoff_datetime'], format='%Y-%m-%d %H:%M:%S')
+
+count = 0
+
 # iterate over each row in the table and send it to Kafka
 for index, row in trips.iterrows():
     # select a subset of columns
@@ -36,6 +51,9 @@ for index, row in trips.iterrows():
 
     # print
     print("Message sent to Kafka: {}".format(message))
+
+    count += 1
+    print(count)
 
     # wait for 0.25 seconds
     time.sleep(0.25)
